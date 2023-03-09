08896e3edc9667fafeae9343d960f0968c5a2195
Bhuvi1410 <120046592+Bhuvi1410@users.noreply.github.com>
2023-03-09 21:13:24 +0530

    Add files via upload

---
+++ console_based_project/MainClass.java
@@ -0,0 +1,10 @@
+package console_based_project;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		//		Scanner scan = new Scanner(System.in);
+		//Object is created for admin class
+		Admin student = new Admin();
+	}
+}
